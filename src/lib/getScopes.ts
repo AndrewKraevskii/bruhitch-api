598f2ce540bc0c5338bda073ae76d3921ac0ed3a
BruhaBruh <drugsho.jaker@gmail.com>
2022-05-02 20:48:45 +0500

    add scopes and refactor subscribe follow

--- src/lib/getScopes.ts
+++ src/lib/getScopes.ts
@@ -1,7 +1,7 @@
 import { TwitchScope } from '../types/twitch';
 
 const getScopes = (): TwitchScope[] => {
-  return [TwitchScope.OpenId, TwitchScope.ReadEmail];
+  return [TwitchScope.OpenId, TwitchScope.ReadEmail, TwitchScope.Subscriptions, TwitchScope.Bits];
 };
 
 export default getScopes;
